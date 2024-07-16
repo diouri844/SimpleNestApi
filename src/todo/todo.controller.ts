1e276b63c5935e998e96e603e3b8b09cde88f608
Chopen <salahiddine.diouri@usmba.ac.ma>
2024-07-16 23:37:28 +0200

    update todo controller

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,4 +1,23 @@
 
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
+import { TodoService } from "./todo.service";
+import { TodoItem } from "./interfaces/todo.interface";
 
 
-export class TodoController { };
+@Controller("Todos")
+export class TodoController {
+    constructor(private TodoProvider: TodoService) { };
+    // time to define routes : 
+    @Post()
+    async createTodo(@Body() TodoPayload: TodoItem) {
+        this.TodoProvider.createTodo(TodoPayload);
+    };
+    @Get()
+    async getTodoList() {
+        this.TodoProvider.retriveAll();
+    };
+    @Get("/:id")
+    async getTodoById(@Param() id: string) {
+        this.getTodoById(id);
+    }
+};
