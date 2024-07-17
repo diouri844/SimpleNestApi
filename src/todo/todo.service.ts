42d9f690a6066d4184b2f5dcf6a6729401cf6502
Chopen <salahiddine.diouri@usmba.ac.ma>
2024-07-17 22:30:06 +0200

    update todo service

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -6,24 +6,34 @@ import { TodoItem } from "./interfaces/todo.interface";
 
 @Injectable()
 export class TodoService {
-    private readonly TodoList: TodoItem[] = [
-        {
+    private readonly TodoList: TodoItem[] = [];
+    constructor() {
+        this.TodoList.push({
             id: "todo1",
             label: "My Todo ",
             description: "bla bla bla ",
             state: "Pending"
-        }
-    ];
+        });
+    }
     // let define some actions :
     createTodo(todo: TodoItem): TodoItem[] {
         this.TodoList.push(todo);
         return this.TodoList;
     };
     findTodo(id: string): TodoItem {
-        return this.TodoList.filter(item => item.id === id)[0];
+        let target: TodoItem = {
+            id: "",
+            label: "",
+            description: "",
+            state: ""
+        };
+        const arr = this.TodoList[0]
+        console.log(arr[0]);
+        return arr[0] as TodoItem;
     };
     retriveAll(): TodoItem[] {
-        return this.TodoList;
+        const response: TodoItem[] = this.TodoList;
+        return response;
     };
     removeItem(id: string): TodoItem[] {
         return this.TodoList.filter(
