e43eb5bfa4f8030fc612d11f33979fd6cb4097d8
Chopen <salahiddine.diouri@usmba.ac.ma>
2024-07-18 22:03:57 +0200

    update todo service

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,43 +1,36 @@
 import { Injectable } from "@nestjs/common";
-import { TodoItem } from "./interfaces/todo.interface";
-
+import { PrismaClient, Todo } from "@prisma/client";
+import * as _ from "lodash";
 
 
 
 @Injectable()
 export class TodoService {
-    private readonly TodoList: TodoItem[] = [];
-    constructor() {
-        this.TodoList.push({
-            id: "todo1",
-            label: "My Todo ",
-            description: "bla bla bla ",
-            state: "Pending"
-        });
-    }
     // let define some actions :
-    createTodo(todo: TodoItem): TodoItem[] {
-        this.TodoList.push(todo);
-        return this.TodoList;
+    private MyPrismaClient = new PrismaClient();
+    async createTodo(todo: Todo): Promise<Todo | null> {
+        const createdTodo: Todo | null = await this.MyPrismaClient.todo.create({
+            data: todo
+        });
+        return createdTodo;
     };
-    findTodo(id: string): TodoItem {
-        let target: TodoItem = {
-            id: "",
-            label: "",
-            description: "",
-            state: ""
-        };
-        const arr = this.TodoList[0]
-        console.log(arr[0]);
-        return arr[0] as TodoItem;
+    async findTodo(id: string): Promise<Todo | null> {
+        const todoTarget: Todo | null = await this.MyPrismaClient.todo.findUnique({
+            where: {
+                id
+            }
+        });
+        return todoTarget;
     };
-    retriveAll(): TodoItem[] {
-        const response: TodoItem[] = this.TodoList;
+    async retriveAll(): Promise<Todo[]> {
+        const response: Todo[] = await this.MyPrismaClient.todo.findMany();
         return response;
     };
-    removeItem(id: string): TodoItem[] {
-        return this.TodoList.filter(
-            item => item.id !== id
-        );
+    async removeItem(id: string): Promise<boolean> {
+        return await this.MyPrismaClient.todo.delete({
+            where: {
+                id
+            }
+        }) != null
     }
 };
