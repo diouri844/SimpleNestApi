d5226923d529c4c3c0f0d4571c9d693a8413e2d9
Chopen <salahiddine.diouri@usmba.ac.ma>
2024-07-18 22:03:41 +0200

    update todo controller

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -2,28 +2,42 @@
 import { Body, Controller, Get, Param, Post } from "@nestjs/common";
 import { TodoService } from "./todo.service";
 import { TodoItem } from "./interfaces/todo.interface";
-
+import { Todo } from "@prisma/client";
 
 @Controller("todos")
 export class TodoController {
     constructor(private TodoProvider: TodoService) { };
     // time to define routes : 
     @Post()
-    async createTodo(@Body() TodoPayload: TodoItem) {
-        return this.TodoProvider.createTodo(TodoPayload);
+    async createTodo(@Body() TodoPayload: Todo) {
+        const result: Todo | null = await this.TodoProvider.createTodo(TodoPayload);
+        // time to check 
+        if (!result) {
+            return {
+                message: "Server can not create new todo"
+            }
+        };
+        return {
+            message: "New Todo Created Successufully",
+            data: result
+        }
     };
     @Get("/all")
     async getTodoList() {
+        const todoList: Todo[] = await this.TodoProvider.retriveAll();
         return {
             message: "All todo retrived successfully",
-            data: this.TodoProvider.retriveAll()
+            data: todoList
         };
     };
     @Get("/item/:id")
-    async getTodoById(@Param() id: string) {
-        console.log(id);
-        const res: TodoItem = this.TodoProvider.findTodo(id);
-        console.log(res);
+    async getTodoById(@Param() params: { id: string }) {
+        const res: Todo | null = await this.TodoProvider.findTodo(params.id);
+        if (!res) {
+            return {
+                message: "Server can not find  todo with ref : " + params.id
+            }
+        }
         return {
             message: "Todo Item retrived successfully",
             data: res
